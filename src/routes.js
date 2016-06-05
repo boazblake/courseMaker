33d6b044cd1edb04113587f6bb1ff74264b93869
boazblake <boazblake@gmail.com>
2016-06-05 07:59:09 -0500

    thunk and mock API working

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import AboutPage from './components/about/aboutPage';
 import HomePage from './components/home/homePage';
-import coursesPage from './components/course/coursesPage';
+import coursesPage from './components/course/CoursesPage';
 
 
 export default(
