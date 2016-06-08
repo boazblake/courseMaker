c8096e738521fdb1301fee6d488b349fed32d745
boazblake <boazblake@gmail.com>
2016-06-07 20:04:19 -0500

    adding text to input

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,10 +5,32 @@ export function loadCoursesSuccess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(courses) {
+  return {type: types.CREATE_COURSES_SUCCESS, courses};
+}
+
+export function updateCoursesSuccess(courses) {
+  return {type: types.UPDATE_COURSES_SUCCESS, courses};
+}
+
+
+
 export function loadCourses(){
   return function(dispatch) {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+
+export function saveCourse(course){
+  return function(dispatch, getState) {
+    return courseApi.saveCourse().then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) : 
+        dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
