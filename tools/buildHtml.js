a12eac38053dedc1b28f98fc07273c61e6f36d58
boazblake <boazblake@gmail.com>
2016-06-14 08:35:32 -0500

    html build written

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,2 +1,23 @@
 import fs form 'fs';
 import cheerio from 'cheerio';
+import colors from 'colors';
+
+/*esling-disable no-console*/
+
+fs.readFile('src/index.html', 'utf8', (err, markup) => {
+  if (err) {
+    return console.log(err.bold.red);
+  }
+
+  const $ = cheerio.load(markup);
+
+  //since a seperarte stylesheet is only utelized for the productuion build, need to dynamicly
+  $(head).prepend('<link rel="stylesheet" href="styles.css">');
+
+  fs.writeFile('dist/index.html', $.html(), 'utf8', (err) => {
+    if (err) {
+      return console.log(err.bold.red);
+    }
+    console.log('index.html written to /dist successffully',rainbow);
+  });
+});
